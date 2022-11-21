0ca5164f6a244f049fc9158d5bf7ae76d376a50d
OGOSHI VALENTINE JOHN <99398711+Agyakoji@users.noreply.github.com>
2022-11-21 23:18:12 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Convert a numeric string to a number
+                // SOLUTION
+// Declare a function
+function stringToNumber(numStr){
+    // converting numeric string to number using Number method
+    let strToNum = Number(numStr)
+    return strToNum
+}
+console.log(stringToNumber('250'))
+
+
+
+
+
+
+
+
+
+
+
+
+            // Using function expression
+// const stringToNumber = function(str){
+//     let strToNum = Number(str)
+//     return strToNum
+// } 
+// alert(stringToNumber('250'))
+
+
+            // Using arrow function
+// const stringToNumber = str => {
+//     let strToNum = Number(str)
+//     return strToNum
+// } 
+// alert(stringToNumber('250'))
